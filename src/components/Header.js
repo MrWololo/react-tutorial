eef28a5226b807bc827eaa1c210943504089bb08
MrWololo <martin_pedrosa@hotmail.com>
2021-09-12 11:57:05 -0300

    FrontEnd done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import PropTypes from "prop-types";
+
 import Button from "./Button";
 
-const Header = (props) => {
-  const onClick = (e) => {
-    console.log(e);
-  };
-
+const Header = ({ title, onAdd, showAdd }) => {
   return (
-    <header>
-      <h1 style={headingStyle}>{props.title}</h1>
-      <Button color="green" text="Hello" onClick={onClick} />
+    <header className="header">
+      <h1 style={headingStyle}>{title}</h1>
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
     </header>
   );
 };
